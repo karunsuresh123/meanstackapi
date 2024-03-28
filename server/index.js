2cf45cb00dab3e3e454a0c77510f24f87080b3f2
Karun Suresh Raj <karun.suresh@gmail.com>
2024-03-27 20:46:25 -0700

    initial commit

--- server/index.js
+++ server/index.js
@@ -39,9 +39,6 @@ app.get('/', (req, res) => {
     //res.sendFile(path.join(__dirname, './ui/build/index.html'));
     res.send({message: 'Hello from API !'});
 });
-
-
-
 app.listen(port, () => {
     console.log(`Server listening on the port  ${port}`);
 })
